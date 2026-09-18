a6266530781a50eda9a07a56280408b19756efc3
Christophe Savard <christophe_savard@hotmail.ca>
2026-09-18 13:07:54 -0400

    Add root command

---
+++ Challenge.CLI/ChallengeCommand.cs
@@ -0,0 +1,19 @@
+﻿using DotMake.CommandLine;
+
+namespace Challenge.CLI;
+
+/// <summary>
+/// Challenge Solver Interface
+/// </summary>
+[CliCommand(Description = "Challenge Solver Interface")]
+public sealed class ChallengeCommand : ICliRunWithContext
+{
+    /// <inheritdoc />
+    public void Run(CliContext cliContext)
+    {
+        cliContext.ShowHelp();
+#if DEBUG
+        cliContext.ShowHierarchy();
+#endif
+    }
+}
