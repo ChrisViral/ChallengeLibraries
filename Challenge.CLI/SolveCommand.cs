a4bd70334a3ff807c1a3fb61eed20a0cd6ff12be
Christophe Savard <christophe_savard@hotmail.ca>
2026-09-18 13:08:09 -0400

    Add solve command with input fetching

---
+++ Challenge.CLI/SolveCommand.cs
@@ -0,0 +1,54 @@
+﻿using CSharpFunctionalExtensions;
+using DotMake.CommandLine;
+using Microsoft.Extensions.Logging;
+
+namespace Challenge.CLI;
+
+/// <summary>
+/// Solve a specific challenge instance
+/// </summary>
+[CliCommand(Description = "Solve a specific challenge instance", Name = "solve", Parent = typeof(ChallengeCommand))]
+public sealed partial class SolveCommand(ILogger<SolveCommand> logger, IInputFetcher inputFetcher) : ICliRunAsyncWithContextAndReturn
+{
+    /// <summary>
+    /// Challenge year
+    /// </summary>
+    [CliArgument(Description = "Challenge year")]
+    public int Year { get; set; }
+
+    /// <summary>
+    /// Challenge day
+    /// </summary>
+    [CliArgument(Description = "Challenge day")]
+    public int Day { get; set; }
+
+    /// <summary>
+    /// Challenge module
+    /// </summary>
+    [CliOption(Description = "Challenge module")]
+    public string Module { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Logger instance
+    /// </summary>
+    private ILogger Logger { get; } = logger;
+
+    /// <summary>
+    /// Input fetcher instance
+    /// </summary>
+    private IInputFetcher InputFetcher { get; } = inputFetcher;
+
+    /// <inheritdoc />
+    public async Task<int> RunAsync(CliContext cliContext)
+    {
+        LogFetchingInput(this.Logger, this.Year, this.Day, string.IsNullOrEmpty(this.Module) ? string.Empty : $" ({this.Module})");
+        Result<string> result = await this.InputFetcher.Fetch(this.Year, this.Day, this.Module, cliContext.CancellationToken).ConfigureAwait(false);
+        if (!result.TryGetValue(out string? data))
+        {
+            LogInputFetchFailed(this.Logger, this.Year, this.Day, string.IsNullOrEmpty(this.Module) ? string.Empty : $" ({this.Module})", result.Error);
+            return 1;
+        }
+
+        return 0;
+    }
+}
---
+++ Challenge.CLI/SolveCommand.logger.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Challenge.CLI
+{
+    public partial class SolveCommand
+    {
+        [LoggerMessage(LogLevel.Error, "Could not fetch input for Challenge {Year} {Day}{Module}: {Error}")]
+        static partial void LogInputFetchFailed(ILogger logger, int year, int day, string module, string error);
+
+        [LoggerMessage(LogLevel.Information, "Fetching input for for Challenge {Year} {Day}{Module}")]
+        static partial void LogFetchingInput(ILogger logger, int year, int day, string module);
+    }
+}
