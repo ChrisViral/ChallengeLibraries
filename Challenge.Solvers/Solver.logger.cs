46e4aad16b277e4dc611ad4759e7945150a95911
Christophe Savard <christophe_savard@hotmail.ca>
2026-09-18 23:59:49 -0400

    Change logger class to file-scoped namespace

--- Challenge.Solvers/Solver.logger.cs
+++ Challenge.Solvers/Solver.logger.cs
@@ -1,19 +1,18 @@
 ﻿using Microsoft.Extensions.Logging;
 
-namespace Challenge.Solvers
+namespace Challenge.Solvers;
+
+public partial class Solver
 {
-    public partial class Solver
-    {
-        [LoggerMessage(LogLevel.Information, "Part {Part}: {Answer}")]
-        static partial void LogPartAnswer(ILogger logger, int part, string answer);
+    [LoggerMessage(LogLevel.Information, "Part {Part}: {Answer}")]
+    static partial void LogPartAnswer(ILogger logger, int part, string answer);
 
-        [LoggerMessage(LogLevel.Information, "Time: {Time}\n")]
-        static partial void LogPartTime(ILogger logger, string time);
+    [LoggerMessage(LogLevel.Information, "Time: {Time}\n")]
+    static partial void LogPartTime(ILogger logger, string time);
 
-        [LoggerMessage(LogLevel.Information, "{Message}")]
-        static partial void LogMessage(ILogger logger, object message);
+    [LoggerMessage(LogLevel.Information, "{Message}")]
+    static partial void LogMessage(ILogger logger, object message);
 
-        [LoggerMessage(LogLevel.Information, "Total elapsed time: {Elapsed}")]
-        static partial void LogElapsed(ILogger logger, string elapsed);
-    }
+    [LoggerMessage(LogLevel.Information, "Total elapsed time: {Elapsed}")]
+    static partial void LogElapsed(ILogger logger, string elapsed);
 }
