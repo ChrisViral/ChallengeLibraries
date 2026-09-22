56b756a4dbe8af5225f2725a3d2c264067976054
Christophe Savard <christophe_savard@hotmail.ca>
2026-09-22 15:06:00 -0400

    Add generator test project

---
+++ Challenge.Generator.Test/GeneratorTest.cs
@@ -0,0 +1,21 @@
+﻿using Challenge.Solvers;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Logging;
+
+namespace Challenge.Generator.Test;
+
+[Solver(2000, 1), UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+internal sealed partial class GeneratorTest : Solver
+{
+    /// <inheritdoc />
+    public GeneratorTest(string input, ILogger logger) : base(input, logger) { }
+
+    [Part(1)]
+    private static void RunPart1() { }
+
+    [Part(2)]
+    private static void RunPart2() { }
+
+    [Part(3)]
+    private static void RunPart3() { }
+}
