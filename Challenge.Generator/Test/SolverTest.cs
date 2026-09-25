08c2d9f674b7e2598f0373e4d1e3bda19d3165a3
Christophe Savard <christophe_savard@hotmail.ca>
2026-09-24 20:32:32 -0400

    Make test folder a subfolder of the generator folder

---
+++ Challenge.Generator/Test/SolverTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Challenge.Solvers;
+using CSharpFunctionalExtensions;
+
+namespace Challenge.Generator.Test;
+
+[Solver(2000, 1)]
+[SuppressMessage("Performance", "CA1822:Mark members as static")]
+internal sealed partial class SolverTest : Solver
+{
+    [Part(1)]
+    private void RunPart1() { }
+
+    [Part(2)]
+    private void RunPart2() { }
+
+    [Part(3)]
+    private void RunPart3() { }
+}
+
+[Solver(2001, 2)]
+[SuppressMessage("Performance", "CA1822:Mark members as static")]
+internal sealed partial class SolverTest2 : Solver
+{
+    [Part(1)]
+    private void RunPart1() { }
+
+    [Part(2)]
+    private void RunPart2() { }
+
+    [Part(3)]
+    private void RunPart3() { }
+}
+
+[SolverTable]
+internal partial class SolverTableTest : ISolverResolver
+{
+    /// <inheritdoc />
+    public string ChallengeName { get; } = "Test";
+
+    /// <inheritdoc />
+    public Task<Result<string>> FetchInput(SolverData data, CancellationToken token = default)
+    {
+        return null!;
+    }
+
+    /// <inheritdoc />
+    public Task<Result> SubmitAnswer(string answer, SolverData data, CancellationToken token = default)
+    {
+        return null!;
+    }
+}
